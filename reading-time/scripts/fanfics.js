1de2a3e782ff3838a235014f1294eb8d70c3a96f
Valen DeLeon <valeriedeleon4521@gmail.com>
2023-11-29 16:20:31 -0800

    Planning with comments!

---
+++ reading-time/scripts/fanfics.js
@@ -0,0 +1,24 @@
+/*
+    AO3 DOM INSPECTION 🔎
+
+    __TEXT LOCATION__
+    <div class="userstuff">
+        <p>...</p>
+        <p>...</p>
+        <p>...</p>
+        ...
+    </div>
+
+    __INJECTION__
+    <div id="worksin">
+        <div class="preface group">
+            <h2 class="title heading"> ... </h2>
+            <h3 class="byline heading">
+                <a rel="author" href="...">...</a>
+            </h3>
+            *stab stab go here*
+            <div class="summary module">...</div>
+            ...
+        </div>
+    </div>
+*/
